b8268379ac3910a679854f4649cf06195cb9e1eb
Sean Gordon <nodrog.naes@gmail.com>
2021-12-16 18:10:12 -0500

    add and remove tracks

--- jammin/src/Components/App/App.js
+++ jammin/src/Components/App/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
-import { Playlist } from '../Playlist/Playlist';
+import { PlayList } from '../PlayList/PlayList';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,36 +12,45 @@ class App extends React.Component {
     {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
     {name: 'name3', artist: 'artist2', album: 'album3', id: 3}],
     playlistName: 'Jamz',
-    playlistTracks: [{name: 'plname1', artist: 'plartist1', album: 'plalbum1', id: 1}, 
-    {name: 'plname2', artist: 'plartist2', album: 'plalbum2', id: 2},
-    {name: 'plname3', artist: 'plartist2', album: 'plalbum3', id: 3}] 
+    playlistTracks: [{name: 'plname1', artist: 'plartist1', album: 'plalbum1', id: 4}, 
+    {name: 'plname2', artist: 'plartist2', album: 'plalbum2', id: 5},
+    {name: 'plname3', artist: 'plartist2', album: 'plalbum3', id: 6}] 
     };
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   };
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
     if (tracks.find(savedTrack =>
       savedTrack.id === track.id)) {
-        return;
-      }
+      return;
+    }
     
     tracks.push(track);
+    this.setState({ playlistTracks: tracks })
+  };
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
     this.setState({ playlistTracks: tracks })
   }
 
   render() {
     return (
       <div>
-        <h1>Ja<span className="highlight">mmm</span>in</h1>
+        <h1>&#9836;Ja<span className="highlight">mmm</span>in</h1>
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} 
               onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName}
-              playlistTracks={this.state.playlistTracks}/>
+            <PlayList playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
--- jammin/src/Components/Playlist/Playlist.js
+++ jammin/src/Components/Playlist/Playlist.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import './Playlist.css';
+import './PlayList.css';
 import { TrackList } from '../TrackList/TrackList';
 
-export class Playlist extends React.Component {
+export class PlayList extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"}/>
-                < TrackList tracks={this.props.playlistTracks}/>
+                <input value={this.props.playlistName}/>
+                <TrackList tracks={this.props.playlistTracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
