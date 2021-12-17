4dd69ca181ab56322ac955e2fd4b5a7497162c14
Sean Gordon <nodrog.naes@gmail.com>
2021-12-17 14:01:31 -0500

    searchbar query props

--- jammin/src/Components/App/App.js
+++ jammin/src/Components/App/App.js
@@ -20,6 +20,8 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   };
 
   addTrack(track) {
@@ -44,19 +46,28 @@ class App extends React.Component {
     this.setState({ playlistName: name })
   }
 
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(query) {
+    console.log(query);
+  }
+
   render() {
     return (
       <div>
         <h1>&#9836;Ja<span className="highlight">mmm</span>in</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} 
               onAdd={this.addTrack}/>
             <PlayList playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack} 
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName} 
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
